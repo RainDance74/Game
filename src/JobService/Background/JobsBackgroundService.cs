d97ba5a61c4da7f833edf271c8ad655500d9c84e
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-03-08 22:37:08 +0500

    Add job background services

---
+++ src/JobService/Background/JobsBackgroundService.cs
@@ -0,0 +1,45 @@
+﻿using JobService.Contracts;
+
+using MassTransit;
+
+namespace JobService.Background;
+
+public class JobsBackgroundService(ILogger<JobsBackgroundService> _logger,
+    ISendEndpointProvider _sendEndpointProvider)
+    : BackgroundService
+{
+    private readonly Mutex _mutex = new(false, "Global\\JobsBackgroundService_Mutex");
+    private readonly Random _random = new();
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation(
+            "Jobs Background Service is launched.");
+
+        _mutex.WaitOne();
+
+        _logger.LogInformation(
+            "Jobs Background Service is working.");
+
+        while(!stoppingToken.IsCancellationRequested)
+        {
+            await CreateRandomJob(stoppingToken);
+
+            await Task.Delay(TimeSpan.FromMinutes(_random.Next(1, 10)), stoppingToken);
+        }
+    }
+
+    public async Task CreateRandomJob(CancellationToken stoppingToken) =>
+        await _sendEndpointProvider.Send<CreateJob>(new(_random.Next(300, 3000)), stoppingToken);
+
+    public override async Task StopAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation(
+            "Jobs Background Service is stopping.");
+
+        _mutex.ReleaseMutex();
+        _mutex.Dispose();
+
+        await base.StopAsync(stoppingToken);
+    }
+}
---
+++ src/JobService/Background/PayrollBackgroundService .cs
@@ -0,0 +1,67 @@
+﻿using JobService.Database;
+using JobService.Database.Models;
+
+using MassTransit;
+
+using Microsoft.EntityFrameworkCore;
+
+using UserService.Contracts;
+
+namespace JobService.Background;
+
+public class PayrollBackgroundService(IServiceProvider _serviceProvider,
+    ILogger<PayrollBackgroundService> _logger,
+    ISendEndpointProvider _sendEndpointProvider)
+    : BackgroundService
+{
+    private readonly Mutex _mutex = new(false, "Global\\PayrollBackgroundService_Mutex");
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation(
+            "Payroll Background Service is launched.");
+
+        _mutex.WaitOne();
+
+        _logger.LogInformation(
+            "Payroll Background Service is working.");
+
+        while(!stoppingToken.IsCancellationRequested)
+        {
+            using(IServiceScope scope = _serviceProvider.CreateScope())
+            {
+                AppDbContext dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                await Payroll(dbContext, stoppingToken);
+            }
+
+            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+        }
+    }
+
+    public async Task Payroll(AppDbContext context, CancellationToken stoppingToken)
+    {
+        Job[] jobs = await context.Jobs
+            .AsNoTracking()
+            .ToArrayAsync(stoppingToken);
+
+        foreach(Job job in jobs)
+        {
+            await Task.WhenAll(job.Workers.Select(w => Payroll(w, job.Salary)));
+        }
+
+        async Task Payroll(Guid worker, decimal salary) =>
+            await _sendEndpointProvider.Send<IncrementUserBalance>(new(worker, salary), stoppingToken);
+    }
+
+    public override async Task StopAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation(
+            "Payroll Background Service is stopping.");
+
+        _mutex.ReleaseMutex();
+        _mutex.Dispose();
+
+        await base.StopAsync(stoppingToken);
+    }
+}
