f5227dffa6daa18b43e5094af50961b7920ae2ba
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-03-08 18:34:31 +0500

    Add project files.

---
+++ src/OrchestratorService/Program.cs
@@ -0,0 +1,13 @@
+using OrchestratorService;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddHostedService<Worker>();
+
+        var host = builder.Build();
+        host.Run();
+    }
+}
