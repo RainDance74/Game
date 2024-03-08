294cf09a1ec9051591ed00b18f2c1be8974436d5
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2024-03-08 22:41:38 +0500

    Configure startup for user service

--- src/UserService/Program.cs
+++ src/UserService/Program.cs
@@ -1,6 +1,11 @@
 using System.Reflection;
 
+using MassTransit;
+
+using Microsoft.EntityFrameworkCore;
+
 using UserService.Configurations;
+using UserService.Database;
 
 internal class Program
 {
@@ -18,10 +23,31 @@ internal class Program
 
         builder.ConfigureServices((hostContext, services) =>
         {
+            services.AddDbContext<AppDbContext>((sp, options) =>
+            {
+                var connectionString = hostContext.Configuration.GetConnectionString("DefaultConnection");
+
+                options.UseNpgsql(connectionString);
+            });
+
             IConfigurationSection rabbitMqSection = hostContext.Configuration.GetSection("RabbitMqConfiguration");
             RabbitMqConfiguration? rabbitMqConfig = rabbitMqSection.Get<RabbitMqConfiguration>();
 
-            // TODO: Configure services
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumers(Assembly.GetExecutingAssembly());
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.ConfigureEndpoints(context);
+
+                    cfg.Host(rabbitMqConfig?.Hostname, rabbitMqConfig?.VirtualHost, h =>
+                    {
+                        h.Username(rabbitMqConfig?.Username);
+                        h.Password(rabbitMqConfig?.Password);
+                    });
+                });
+            });
         });
 
         return builder;
